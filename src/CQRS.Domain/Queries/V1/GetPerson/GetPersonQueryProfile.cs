abc337de4c29dbe71a3484d09c25449dce46eb09
Willian Marioti <willian.marioti@gmail.com>
2023-05-22 11:27:01 -0300

    Correção do get por id.

--- src/CQRS.Domain/Queries/V1/GetPerson/GetPersonQueryProfile.cs
+++ src/CQRS.Domain/Queries/V1/GetPerson/GetPersonQueryProfile.cs
@@ -11,8 +11,8 @@ public class GetPersonQueryProfile : Profile
             .ForMember(fieldOutput => fieldOutput.Cpf, option => option
                 .MapFrom(input => input.Cpf.Value.FormatCpf()))
             .ForMember(fieldOutput => fieldOutput.Name, option => option
-                .MapFrom(input => input.Name.Value.FormatCpf()))
+                .MapFrom(input => input.Name.Value))
             .ForMember(fieldOutput => fieldOutput.Email, option => option
-                .MapFrom(input => input.Email.Value.FormatCpf()));
+                .MapFrom(input => input.Email.Value));
     }
 }
